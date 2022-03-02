b384c0d491e7c3698e29c7e5a9836020f25f8084
Claudia Maia <maiaandradec@gmail.com>
2022-03-02 19:25:08 -0300

    feat: loop de repetição

--- menu.go
+++ menu.go
@@ -2,26 +2,30 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo logs:")
-	case 0:
-		fmt.Println("Saindo do programa.")
-		os.Exit(0)
-	default: 
-		fmt.Println("Não conheço este comando")	
-		os.Exit(-1)	
+	for {
+		exibeMenu()
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs:")
+		case 0:
+			fmt.Println("Saindo do programa.")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -41,8 +45,20 @@ func leComando() int {
 	return comandoLido
 }
 
-func exibeMenu(){
+func exibeMenu() {
 	fmt.Println("1- Iniciar monitoramento")
 	fmt.Println("2- Exibir logs")
 	fmt.Println("0- Sair do programa")
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	//site := "https://www.questoesestrategicas.com.br/"
+	site := "https://random-status-code.herokuapp.com/"
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Sttus code: ", resp.StatusCode)
+	}
 }
